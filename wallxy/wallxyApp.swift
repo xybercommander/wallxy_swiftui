98c9dcc59526062367e85ecbfc27cced562ff061
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2024-05-31 12:16:05 +0530

    Initial Commit

---
+++ wallxy/wallxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallxyApp.swift
+//  wallxy
+//
+//  Created by Samrat Mukherjee on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct wallxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
